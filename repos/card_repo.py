2195358fc473b40c9a892eb81b4871d56d6b590c
Bryan Yancey <yanceybryand@ymail.com>
2022-05-20 18:40:13 -0500

    Card Repos and Services

---
+++ repos/card_repo.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class CardRepo(ABC):
+    @abstractmethod
+    def create_card(self, card):
+        pass
+
+    @abstractmethod
+    def update_card(self, update):
+        pass
+
+    @abstractmethod
+    def get_card(self, card_id):
+        pass
+
+    @abstractmethod
+    def all_card(self):
+        pass
+
+    @abstractmethod
+    def delete_card(self, card_id):
+        pass
---
+++ services/CardServ.py
@@ -0,0 +1,21 @@
+from repository.card_repo import CardRepo
+
+
+class CardServ:
+    def __init__(self, card_repo: CardRepo):
+        self.card_repo = card_repo
+
+    def create_card(self, card):
+        return self.card_repo.create_card(card)
+
+    def update_card(self, update):
+        return self.card_repo.update_card(update)
+
+    def get_card(self, card_id):
+        return self.card_repo.get_card(card_id)
+
+    def all_card(self):
+        return self.card_repo.all_card()
+
+    def delete_card(self, card_id):
+        return self.card_repo.delete_card(card_id)
